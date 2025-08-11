cabf6f95a7c4260690d050c007207d3b8163a98b
Paul Irem <dev.paulirem@gmail.com>
2025-08-11 12:08:49 +0100

    chore

--- src/learning/interface.ts
+++ src/learning/interface.ts
@@ -5,13 +5,6 @@ export interface ILearningComponent {
   renderSummary(): void;
 }
 
-// I will also keep the LearningComponent class here for now, as it's the implementation of the contract.
-// The user's instructions are a bit ambiguous on this point.
-// "interface doesn't export only declare interface for the component to implement"
-// This could mean that the file should only contain the interface declaration.
-// Or it could mean that the file should not export the data entities.
-// I will keep the component class here for now, and I can move it later if the user clarifies.
-
 export class LearningComponent implements ILearningComponent {
     loadQuestions() {
       console.log("Loading questions...");
